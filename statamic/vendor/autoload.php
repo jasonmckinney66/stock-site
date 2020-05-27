f64b617e26f31588da8ad2e03102d1d88ce900b7
Jason McKinney <j@jasonmckinney.com>
2020-05-27 12:50:20 -0700

    Jason upgraded Statamic.

--- statamic/vendor/autoload.php
+++ statamic/vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInit362ff105fbeec6fd51a427555424dadd::getLoader();
+return ComposerAutoloaderInit546eac093543c7e66ef512ef31e9891d::getLoader();
