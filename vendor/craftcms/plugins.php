1d46037b2dc88ed7cbef9e95719c272ce15d647f
Jason McKinney <j@jasonmckinney.com>
2024-01-18 21:26:10 -0800

    Added S3 plugin. Updated entry template.

--- vendor/craftcms/plugins.php
+++ vendor/craftcms/plugins.php
@@ -195,4 +195,21 @@ return array (
     'developerEmail' => 'support@craftcms.com',
     'documentationUrl' => 'https://docs.craftcms.com/feed-me/v4/',
   ),
+  'craftcms/aws-s3' => 
+  array (
+    'class' => 'craft\\awss3\\Plugin',
+    'basePath' => $vendorDir . '/craftcms/aws-s3/src',
+    'handle' => 'aws-s3',
+    'aliases' => 
+    array (
+      '@craft/awss3' => $vendorDir . '/craftcms/aws-s3/src',
+    ),
+    'name' => 'Amazon S3',
+    'version' => '1.3.2',
+    'description' => 'Amazon S3 integration for Craft CMS',
+    'developer' => 'Pixel & Tonic',
+    'developerUrl' => 'https://pixelandtonic.com/',
+    'developerEmail' => 'support@craftcms.com',
+    'documentationUrl' => 'https://github.com/craftcms/aws-s3/blob/master/README.md',
+  ),
 );
